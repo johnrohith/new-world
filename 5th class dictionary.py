82295bb8c3320730df4be5ee6a3a6845fe6a8d56
johnrohith <35676393+johnrohith@users.noreply.github.com>
2018-02-05 09:51:16 +0530

    Add files via upload

---
+++ 5th class dictionary.py
@@ -0,0 +1,13 @@
+my_dict = {'name':'John', 'age': 19}
+
+# update value
+my_dict['age'] = 19
+
+#Output: {'age': 19, 'name': 'John'}
+print(my_dict)
+
+# add item
+my_dict['address'] = 'CMRU'  
+
+# Output: {'address': 'Downtown', 'age': 19, 'name': 'John'}
+print(my_dict['name'])
