89df268aed7afa5e1466d4312549eba99a99a426
johnrohith <35676393+johnrohith@users.noreply.github.com>
2018-02-14 08:56:58 +0530

    Add files via upload

---
+++ rock paper scisors.py
@@ -0,0 +1,20 @@
+Python 3.6.3 (default, Oct  3 2017, 21:45:48) 
+[GCC 7.2.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: /home/cs2017a225/rock paper .py ==================
+Rock, Paper, Scissors?
+That's not a valid play. Check your spelling!
+Rock, Paper, Scissors?
+That's not a valid play. Check your spelling!
+Rock, Paper, Scissors?
+================== RESTART: /home/cs2017a225/rock paper .py ==================
+Rock, Paper, Scissors?rock
+That's not a valid play. Check your spelling!
+Rock, Paper, Scissors?Rock
+You lose! Paper covers Rock
+Rock, Paper, Scissors?Paper
+You lose! Scissors cut Paper
+Rock, Paper, Scissors?
+================== RESTART: /home/cs2017a225/rock paper .py ==================
+Rock, Paper, Scissors?
